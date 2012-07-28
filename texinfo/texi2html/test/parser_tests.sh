86342b1d70554d0ab60c0092f387223b6dc7b067
pertusus <pertusus@39fee189-59d7-47db-b5d4-205258b72aed>
2012-07-28 13:28:11 +0000

    Scripts and driving files to run the parser on the texi2html tests.
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@4726 39fee189-59d7-47db-b5d4-205258b72aed

---
+++ texinfo/texi2html/test/parser_tests.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+# Copyright (C) 2010 Free Software Foundation, Inc.
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.
+
+if [ z"$srcdir" = 'z' ]; then
+  srcdir=.
+fi
+
+command=run_parser.sh
+
+if [ z"$1" = 'zall' ]; then
+  command=run_parser_all.sh
+  shift
+fi
+
+arg=
+if [ z"$1" = 'z-clean' ]; then
+  arg='-clean'
+  shift
+elif [ z"$1" = 'z-copy' ]; then
+  arg='-copy'
+  shift
+fi
+
+if [ z"$1" = 'zall' ]; then
+  command=run_parser_all.sh
+  shift
+fi
+
+failed=0
+while [ z"$1" != 'z' ]; do
+  dir=$1
+  shift
+  [ -d "$dir" ] || mkdir $dir
+  (export srcdir_test=$dir; cd "$dir" && ../"$srcdir"/"$command" $arg)
+  result=$?
+  echo "$dir: $result"
+  [ $result != 0 ] && failed=1
+done
+
+exit $failed
