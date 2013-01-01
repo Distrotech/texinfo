fa5612445e82e7456030f0be532e5027d6973cbf
karl <karl@39fee189-59d7-47db-b5d4-205258b72aed>
2013-01-01 19:31:57 +0000

    2013
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@5012 39fee189-59d7-47db-b5d4-205258b72aed

--- texinfo/install-info/install-info.c
+++ texinfo/install-info/install-info.c
@@ -1,8 +1,9 @@
 /* install-info -- merge Info directory entries from an Info file.
-   $Id: install-info.c,v 1.20 2012-12-31 19:02:57 karl Exp $
+   $Id: install-info.c,v 1.21 2013-01-01 19:31:57 karl Exp $
 
    Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-   2005, 2007, 2008, 2009, 2010, 2011, 2012 Free Software Foundation, Inc.
+   2005, 2007, 2008, 2009, 2010, 2011, 2012, 2013
+   Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -2142,7 +2143,7 @@ main (int argc, char *argv[])
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n"),
-              "2012");
+              "2013");
           exit (EXIT_SUCCESS);
 
         case 'W':
