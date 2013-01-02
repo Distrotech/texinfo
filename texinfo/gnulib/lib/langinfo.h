d22070ca07275e8ca0fd7920629fb66a044f2699
karl <karl@39fee189-59d7-47db-b5d4-205258b72aed>
2013-01-02 01:29:25 +0000

    gnulib-tool --add-import
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@5017 39fee189-59d7-47db-b5d4-205258b72aed

--- texinfo/gnulib/lib/langinfo.h
+++ texinfo/gnulib/lib/langinfo.h
@@ -1,6 +1,6 @@
 /* DO NOT EDIT! GENERATED AUTOMATICALLY! */
 /* Substitute for and wrapper around <langinfo.h>.
-   Copyright (C) 2009-2012 Free Software Foundation, Inc.
+   Copyright (C) 2009-2013 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
