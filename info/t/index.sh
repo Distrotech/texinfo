d8ba1d201d83c657b75dd4f6b23a601014b47d89
gavin <gavin@39fee189-59d7-47db-b5d4-205258b72aed>
2014-04-13 13:52:18 +0000

    copyright notices in info/t
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@5441 39fee189-59d7-47db-b5d4-205258b72aed

--- info/t/index.sh
+++ info/t/index.sh
@@ -1,3 +1,17 @@
+# Copyright (C) 2014 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #!/bin/sh
 
 . t/Init-test.inc
@@ -5,11 +19,11 @@
 
 # Follow an index entry
 
-rm -f t/ginfo-output
 $GINFO -f intera --restore t/index.drib
-kill $PTY_PID
 
 test -f t/ginfo-output || exit 1
-
 # Return non-zero (test failure) if files differ
 diff t/ginfo-output t/node-target
+RETVAL=$?
+
+. t/Cleanup.inc
--- info/t/tab.sh
+++ info/t/tab.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+# Copyright (C) 2014 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 . t/Init-test.inc
 . t/Init-intera.inc
@@ -7,6 +21,7 @@
 $GINFO -f intera --restore t/tab.drib
 
 test -f t/ginfo-output || exit 1
+# Return non-zero (test failure) if files differ
 diff t/ginfo-output t/node-target
 RETVAL=$?
 
