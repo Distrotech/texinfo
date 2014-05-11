b2b2693fe15672687f3621252eb8a995a3533faf
gavin <gavin@39fee189-59d7-47db-b5d4-205258b72aed>
2014-05-11 18:46:19 +0000

    add bodystart info test
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@5554 39fee189-59d7-47db-b5d4-205258b72aed

---
+++ info/t/body-start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Copyright (C) 2014 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+. t/Init-test.inc
+. t/Init-intera.inc
+
+# Check that node headers aren't included in searches
+$GINFO -f body-start --restore $t/body-start.drib
+
+test -f $GINFO_OUTPUT || exit 1
+# Return non-zero (test failure) if files differ
+diff $GINFO_OUTPUT $t/node-target
+RETVAL=$?
+
+. t/Cleanup.inc
+
