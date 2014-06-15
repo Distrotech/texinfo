c0cddede13ee59ac6f7bb646f407b1407d5c0c7a
gavin <gavin@39fee189-59d7-47db-b5d4-205258b72aed>
2014-06-15 15:40:40 +0000

    new test for searching after an Info tag
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@5667 39fee189-59d7-47db-b5d4-205258b72aed

---
+++ info/t/search-after-tag.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Copyright (C) 2014 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+srcdir=${srcdir:-.}
+. $srcdir/t/Init-test.inc
+. $t/Init-intera.inc
+
+# Search for text in a node that appears after an Info tag
+$GINFO -f intera --restore $t/search-after-tag.drib
+
+if ! test -f $GINFO_OUTPUT
+then
+	RETVAL=1
+else
+	# Return non-zero (test failure) if files differ
+	diff $GINFO_OUTPUT $t/node-target
+	RETVAL=$?
+fi
+
+. $t/Cleanup.inc
+
