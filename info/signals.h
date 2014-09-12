8b5e5c926f7c46ff26bd0463c700b449b3e1638f
gavin <gavin@39fee189-59d7-47db-b5d4-205258b72aed>
2014-09-12 17:26:27 +0000

    2014
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@5824 39fee189-59d7-47db-b5d4-205258b72aed

--- info/signals.h
+++ info/signals.h
@@ -1,7 +1,7 @@
 /* signals.h -- header to include system dependent signal definitions.
    $Id$
 
-   Copyright 1993, 1994, 1995, 1997, 2002, 2004, 2007, 2013
+   Copyright 1993, 1994, 1995, 1997, 2002, 2004, 2007, 2013, 2014
    Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
