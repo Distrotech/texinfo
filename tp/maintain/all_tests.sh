a113956877c193817812bf38c57f6c690a0ebf3a
karl <karl@39fee189-59d7-47db-b5d4-205258b72aed>
2015-02-22 22:53:33 +0000

    test results and other updates for distcheck
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@6139 39fee189-59d7-47db-b5d4-205258b72aed

--- tp/maintain/all_tests.sh
+++ tp/maintain/all_tests.sh
@@ -2,7 +2,7 @@
 # $Id$
 # Helper script for tests.
 #
-# Copyright 2015 Free Software Foundation, Inc.
+# Copyright 2011, 2012, 2013, 2014, 2015 Free Software Foundation, Inc.
 #
 # This file is free software; as a special exception the author gives
 # unlimited permission to copy and/or distribute it, with or without
