15a778f65bb73e7d99cf551eb407c2d34f0c681d
pertusus <pertusus@39fee189-59d7-47db-b5d4-205258b72aed>
2015-02-26 15:56:42 +0000

    * tp/tests/t/stdout.sh, tp/tests/t/stdout_split.sh: use $PERL         from defs to call texi2any.pl.  Report from Eli Zaretskii.
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@6158 39fee189-59d7-47db-b5d4-205258b72aed

--- tp/tests/t/stdout_split.sh
+++ tp/tests/t/stdout_split.sh
@@ -5,8 +5,10 @@ LC_ALL=C; export LC_ALL
 
 srcdir=${srcdir:-.}
 
+. ../defs || exit 1
+
 # to regenerate the reference:
 # ../texi2any.pl -o - --plaintext --split chapter formatting/simplest.texi > reference/stdout_split.txt 2>&1
 
-$srcdir/../texi2any.pl -o - --plaintext --split chapter $srcdir/formatting/simplest.texi 2>&1 | diff - $srcdir/reference/stdout_split.txt
+$PERL $srcdir/../texi2any.pl -o - --plaintext --split chapter $srcdir/formatting/simplest.texi 2>&1 | diff - $srcdir/reference/stdout_split.txt
 
