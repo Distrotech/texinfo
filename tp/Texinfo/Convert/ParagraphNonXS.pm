73770431ee56442b87a5b91e463664a8cecf9eeb
gavin <gavin@39fee189-59d7-47db-b5d4-205258b72aed>
2015-11-03 14:51:32 +0000

    update copyright years
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@6751 39fee189-59d7-47db-b5d4-205258b72aed

--- tp/Texinfo/Convert/ParagraphNonXS.pm
+++ tp/Texinfo/Convert/ParagraphNonXS.pm
@@ -1,6 +1,6 @@
-# Paragraph.pm: handle paragraph text.
+# ParagraphNonXS.pm: handle paragraph text.
 #
-# Copyright 2010, 2011, 2012, 2013 Free Software Foundation, Inc.
+# Copyright 2010, 2011, 2012, 2013, 2014, 2015 Free Software Foundation, Inc.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
