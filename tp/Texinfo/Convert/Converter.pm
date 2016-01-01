2de044e6ff02f004830310adaac946676455f9dd
karl <karl@39fee189-59d7-47db-b5d4-205258b72aed>
2016-01-01 18:33:25 +0000

    sync
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@6905 39fee189-59d7-47db-b5d4-205258b72aed

--- tp/Texinfo/Convert/Converter.pm
+++ tp/Texinfo/Convert/Converter.pm
@@ -1,7 +1,7 @@
 # $Id$
 # Converter.pm: Common code for Converters.
 #
-# Copyright 2011, 2012, 2013, 2014, 2015 Free Software Foundation, Inc.
+# Copyright 2011, 2012, 2013, 2014, 2015, 2016 Free Software Foundation, Inc.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
