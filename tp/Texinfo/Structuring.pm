7d71d33ed69bef302bb45bd3f3dd97ce6b7459f7
gavin <gavin@39fee189-59d7-47db-b5d4-205258b72aed>
2016-07-30 19:04:20 +0000

    revert code commited by mistake
    git-svn-id: svn://svn.sv.gnu.org/texinfo/trunk@7283 39fee189-59d7-47db-b5d4-205258b72aed

--- tp/Texinfo/Structuring.pm
+++ tp/Texinfo/Structuring.pm
@@ -1,7 +1,8 @@
 # Structuring.pm: extract informations about a document structure based on the 
 #                 document tree.
 #
-# Copyright 2010, 2011, 2012, 2013, 2014 Free Software Foundation, Inc.
+# Copyright 2010, 2011, 2012, 2013, 2014, 2015, 2016 Free Software Foundation, 
+# Inc.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -964,8 +965,6 @@ sub split_pages ($$)
   my $split = shift;
 
   return undef if (!$elements or !@$elements);
-
-  warn "SPLIT BY $split";
 
   my $split_level;
   if (!$split) {
